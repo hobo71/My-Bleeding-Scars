cd73e918d88fcb820f17d8aca9d31d25a248e382
Cosmin Grozea <cosmin_marcel.grozea@stud.acs.upb.ro>
2016-05-19 00:19:59 +0300

    Spawner prefab
    Adaugare spawner monstrii random care ataca playerul si care pot fi
    atacati de player intr-un anumit range si care dispar cand viata le
    ajunge la zero

---
+++ Assets/Scripts/MainSceneScripts/AttackGorillaScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackGorillaScript : MonoBehaviour {
+
+	public int gorillaId;
+	float health;
+	float jumpHeight = 0.1f;
+
+	float jumpTime;
+	float updTime;
+	float attackDistance = 2.0f;
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		jumpTime = Time.realtimeSinceStartup;
+
+		// Fiecare gorila sare la delta t diferit
+		updTime = Random.Range (0.5f, 1.5f);
+		health = 100.0f;
+
+		//obtine id-ul playerului si verifica distanta si daca e mai mica de x ataca-l
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// fa gorila sa sara
+		if (Time.realtimeSinceStartup - jumpTime > updTime) {
+			jumpTime = Time.realtimeSinceStartup;
+			gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, jumpHeight, 0);
+			jumpHeight = -jumpHeight;
+		}
+
+
+		if (player != null) {
+			float distance = Vector3.Distance (gameObject.transform.position, player.transform.position);
+
+			if (distance < attackDistance) {
+				// Todo apeleaza metoda de getDammage de la player (care o fi aia :)) )
+				Debug.Log ("Gorila " + gorillaId + " ataca playerul");
+			}
+		}
+
+		// Daca health a ajuns la 0 distruge gorila
+		if(health <= 0)
+			Destroy (gameObject);
+	}
+
+	public void getDammage(float dammage)
+	{
+		health -= dammage;
+	}
+
+	void OnMouseDown()
+	{
+		GameObject spawner = GameObject.FindGameObjectWithTag("MonsterSpawner");
+		spawner.GetComponent<MonsterSpawnerScript> ().setDestroyTime (Time.realtimeSinceStartup, gorillaId);
+
+		// nu lua dammage de la player daca nu e in range-ul tau
+		if (player != null) {
+			float distance = Vector3.Distance (gameObject.transform.position, player.transform.position);
+
+			if (distance < attackDistance) {
+				Debug.Log ("Gorila " + gorillaId + " a luat dammage de la player");
+				getDammage (10);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MainSceneScripts/MonsterSpawnerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterSpawnerScript : MonoBehaviour {
+
+	public GameObject gorilla;
+	float minX, maxX;
+	float minZ, maxZ;
+	const int nrInamici = 5;
+	GameObject[] inamici;
+	float[] destroyTime;
+	const float range = 5.0f;
+
+	public void setDestroyTime(float time, int gorillaId)
+	{
+		destroyTime [gorillaId] = time;
+	}
+
+	void Start () {
+		minX = transform.position.x;
+		minZ = transform.position.z;
+
+		maxX = minX + range;
+		maxZ = minZ + range;
+
+		inamici = new GameObject[nrInamici];
+		destroyTime = new float[nrInamici];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < nrInamici; i++) {
+			if (inamici [i] == null) {
+				if (Time.realtimeSinceStartup - destroyTime [i] > 5) {
+					Quaternion rotation = Random.rotation;
+					rotation.x = 0; rotation.z = 0;
+					inamici [i] = (GameObject)Instantiate (gorilla, new Vector3 (Random.Range (minX, maxX), 0, Random.Range (minZ, maxZ)), rotation);
+					inamici [i].GetComponent<AttackGorillaScript>().gorillaId = i;
+				}
+			}
+		}
+	}
+}
